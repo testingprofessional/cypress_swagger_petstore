6545b6f2e9576cdcae0165b9bf24e1a1bd547345
testingprofessional <loeffen19@hotmail.com>
2024-01-24 08:37:49 +0100

    Axe accessibility check added

--- cypress/e2e/swagger_petstore_pet_ui/get_pet_id_ui.cy.js
+++ cypress/e2e/swagger_petstore_pet_ui/get_pet_id_ui.cy.js
@@ -1,11 +1,16 @@
 beforeEach(() => {
   cy.visit('#/')
+  cy.injectAxe()
 })
 
 describe('GET query: Find pet by id', () => {
   it('finds a pet by known id', () => {
     cy.get('#operations-pet-getPetById').click();
     cy.get('.try-out > .btn').click();
+
+    //Axe check button
+    cy.checkA11y('.try-out > .btn')
+
     cy.get('.parameters-col_description > input').type('1');
     cy.get('.execute-wrapper > .btn').click();
     cy.screenshot();
@@ -22,7 +27,7 @@ describe('GET query: Find pet by id', () => {
     cy.get('.parameters-col_description > input').type('1111111');
     cy.get('.execute-wrapper > .btn').click();
     cy.screenshot();
-    
+
     cy.get('.response-col_description > .markdown > p').invoke('text').then((markdownText) => {
       const expectedMarkdownText = 'Error: Not Found';
       expect(markdownText.trim()).to.equal(expectedMarkdownText.trim());
